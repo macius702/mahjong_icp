28143b31c708d1de031efe53e9b67da334a3e9de
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-07-23 12:05:11 +0200

    Fix unordered leaderbord

--- src/mahjong_icp_backend/src/lib.rs
+++ src/mahjong_icp_backend/src/lib.rs
@@ -1,15 +1,13 @@
-
 use ic_cdk_macros;
 
-use std::{cell::RefCell, collections::HashMap};
+use std::{
+    cell::RefCell,
+    collections::{BTreeMap, HashMap},
+};
 
-use candid::{CandidType};
+use candid::CandidType;
 
 use serde::Deserialize;
-
-use std::collections::BinaryHeap;
-use std::cmp::Reverse;
-
 
 thread_local! {
     static STATE: RefCell<State> = RefCell::new(State::default());
@@ -22,31 +20,35 @@ pub struct State {
 }
 
 //Leaderboard is a collection containing best scores for a given board setup
- 
+
 #[derive(Default, CandidType, Clone, Debug)]
 pub struct Leaderboard {
-    pub scores: BinaryHeap<(Reverse<u32>, String)>,
+    pub scores: BTreeMap<u32, String>,
 }
 
 // //Score contains the user name and the score . The score is the shortest time in miliseconds of the user to solve the board
 #[derive(Default, CandidType, Clone, Deserialize, Debug)]
 pub struct Score {
     pub user: String,
-    pub miliseconds : u32,
+    pub miliseconds: u32,
 }
 
-
 #[ic_cdk_macros::query]
-pub fn get_times_by_board(board_setup : String) -> Leaderboard
-{
-    ic_cdk::println!("get_times_by_board: Function called with board_setup: {}", board_setup);
+pub fn get_times_by_board(board_setup: String) -> Leaderboard {
+    ic_cdk::println!(
+        "get_times_by_board: Function called with board_setup: {}",
+        board_setup
+    );
 
     let mut result = Leaderboard::default();
     STATE.with(|state| {
         let state = state.borrow();
         ic_cdk::println!("get_times_by_board: State borrowed");
         if let Some(leaderboard) = state.leaderboards.get(&board_setup) {
-            ic_cdk::println!("get_times_by_board: Found leaderboard for board_setup: {}", board_setup);
+            ic_cdk::println!(
+                "get_times_by_board: Found leaderboard for board_setup: {}",
+                board_setup
+            );
             result = leaderboard.clone();
         }
     });
@@ -57,70 +59,69 @@ pub fn get_times_by_board(board_setup : String) -> Leaderboard
 }
 
 #[ic_cdk_macros::query]
-pub fn get_times() -> HashMap<String, u32> { // gets one time of each board
+pub fn get_times() -> HashMap<String, u32> {
     ic_cdk::println!("get_times: Function called");
-    
-
-    //take from every leaderboard only the first score (if any)
-    // and make an entry of it - leaderbord name, miliseconds
-    // then pack them into a Hashmap
 
     let mut result = HashMap::new();
     STATE.with(|state| {
         let state = state.borrow();
         for (board_setup, leaderboard) in state.leaderboards.iter() {
-            ic_cdk::println!("get_times: Iterating over leaderboards with board_setup: {}", board_setup);
-            if let Some((Reverse(miliseconds), _)) = leaderboard.scores.peek() {
-                ic_cdk::println!("get_times: Found score for board_setup: {}, miliseconds: {}", board_setup, miliseconds);
-                result.insert(board_setup.clone(), *miliseconds);
+            ic_cdk::println!(
+                "get_times: Iterating over leaderboards with board_setup: {}",
+                board_setup
+            );
+            if let Some((&miliseconds, _)) = leaderboard.scores.iter().next() {
+                ic_cdk::println!(
+                    "get_times: Found score for board_setup: {}, miliseconds: {}",
+                    board_setup,
+                    miliseconds
+                );
+                result.insert(board_setup.clone(), miliseconds);
             }
             ic_cdk::println!("get_times: Iteration done");
         }
     });
 
-    //only print full STATE - mtlk todo printing into separate function
     STATE.with(|state| {
-       let state = state.borrow();
-//iterate over leaderboards and print them separately
+        let state = state.borrow();
         for (board_setup, leaderboard) in state.leaderboards.iter() {
             ic_cdk::println!("board_setup: {}", board_setup);
-            for (Reverse(miliseconds), user) in leaderboard.scores.iter() {
+            for (&miliseconds, user) in leaderboard.scores.iter() {
                 ic_cdk::println!("    miliseconds: {}, user: {}", miliseconds, user);
             }
         }
-
-
-       //ic_cdk::println!("get_times: Full state: {:?}", state);
     });
-
-
-
-
 
     ic_cdk::println!("get_times: Function done returning {:?}", result);
 
     result
-    
 }
-
 
 #[ic_cdk_macros::update]
 pub fn set_time(board_setup: String, miliseconds: u32, user: String) {
-    ic_cdk::println!("set_time: Function called with board_setup: {}, miliseconds: {}, user: {}", board_setup, miliseconds, user);
-
+    ic_cdk::println!(
+        "set_time: Function called with board_setup: {}, miliseconds: {}, user: {}",
+        board_setup,
+        miliseconds,
+        user
+    );
 
     STATE.with(|state| {
         let mut state = state.borrow_mut();
         ic_cdk::println!("set_time: State borrowed");
-        let leaderboard = state.leaderboards.entry(board_setup).or_insert(Leaderboard { scores: BinaryHeap::new() });
+        let leaderboard = state
+            .leaderboards
+            .entry(board_setup)
+            .or_insert(Leaderboard {
+                scores: BTreeMap::new(),
+            });
         ic_cdk::println!("set_time: Leaderboard entry created");
 
-        // Push the score into the heap
-        leaderboard.scores.push((Reverse(miliseconds), user.clone()));
-        ic_cdk::println!("set_time: Score pushed");
+        // Insert the score into the map
+        leaderboard.scores.insert(miliseconds, user.clone());
+        ic_cdk::println!("set_time: Score inserted");
     })
 }
-
 #[ic_cdk_macros::update]
 pub fn debug_delete_all_data() {
     ic_cdk::println!("debug_delete_all_data: Function called");
@@ -132,6 +133,5 @@ pub fn debug_delete_all_data() {
         ic_cdk::println!("debug_delete_all_data: State cleared");
     })
 }
-
 
 ic_cdk::export_candid!();
